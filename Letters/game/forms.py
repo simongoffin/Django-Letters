d8a0fd5cfc60b671c959f294385c5f725ebf6110
Goffin Simon <simon.goffin@outlook.com>
2014-04-10 12:07:46 +0200

    Game is operational.

---
+++ Letters/game/forms.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+from django import forms
+
+class LettersForm(forms.Form):
+    max=30
+    arg1 = forms.CharField(label="*", max_length=max)
+    arg2 = forms.CharField(label="*", max_length=1)
+    arg3 = forms.CharField(label="*", max_length=1)
+    arg4 = forms.CharField(label="*", max_length=1)
+    arg5 = forms.CharField(label="*", max_length=1)
+    arg6 = forms.CharField(label="*", max_length=1)
+    arg7 = forms.CharField(label="*", max_length=1)
+    arg8 = forms.CharField(label="*", max_length=1)
+    arg9 = forms.CharField(label="*", max_length=1)
+    arg10 = forms.CharField(label="*", max_length=1)
---
+++ Letters/game/views.py
@@ -0,0 +1,66 @@
+#-*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from game.forms import LettersForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+
+
+lettres=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+voyelles=['a','e','i','o','u','y']
+
+
+# Create your views here.
+
+def auteur(request):
+  text = """<h1>Première page</h1>
+            <p>Simon Goffin</p>"""
+  return HttpResponse(text)
+  
+def auteur2(request):
+  return render(request, 'game/auteur2.html',locals())
+
+def home(request):
+    from core import run
+    if request.method == 'POST':  # S'il s'agit d'une requête POST
+        form = LettersForm(request.POST)  # Nous reprenons les données
+
+        if form.is_valid(): # Nous vérifions que les données envoyées sont valides
+
+            # Ici nous pouvons traiter les données du formulaire
+            arg1 = form.cleaned_data['arg1']
+            arg2 = form.cleaned_data['arg2']
+            arg3 = form.cleaned_data['arg3']
+            arg4 = form.cleaned_data['arg4']
+            arg5 = form.cleaned_data['arg5']
+            arg6 = form.cleaned_data['arg6']
+            arg7 = form.cleaned_data['arg7']
+            arg8 = form.cleaned_data['arg8']
+            arg9 = form.cleaned_data['arg9']
+            arg10 = form.cleaned_data['arg10']
+            tuple=(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,arg9,arg10)
+            resultat=run(tuple)
+            return render(request, 'game/ready.html',locals())
+
+    else: # Si ce n'est pas du POST, c'est probablement une requête GET
+        form = LettersForm()  # Nous créons un formulaire vide
+
+    return render(request, 'game/home.html',locals())
+    
+def random(request):
+    import random
+    data = {'arg1': lettres[random.randrange(0,26)],
+            'arg2': voyelles[random.randrange(0,6)],
+            'arg3': lettres[random.randrange(0,26)],
+            'arg4': lettres[random.randrange(0,26)],
+            'arg5': voyelles[random.randrange(0,6)],
+            'arg6': lettres[random.randrange(0,26)],
+            'arg7': lettres[random.randrange(0,26)],
+            'arg8': voyelles[random.randrange(0,6)],
+            'arg9': lettres[random.randrange(0,26)],
+            'arg10': lettres[random.randrange(0,26)]}
+    form = LettersForm(data)
+    return render(request, 'game/random.html',locals())
+    
+
