d851650a1a3ec72bb087a9b06e4558d89b0b5f3e
Goffin Simon <simon.goffin@outlook.com>
2014-04-19 14:37:46 +0200

    Ajout de l'algorithme space.

--- Letters/score/views.py
+++ Letters/score/views.py
@@ -5,8 +5,9 @@ from challenge.models import Score
 
 
 def score(request):
+    from Algo.Space import space
     liste=Score.objects.order_by('-score','temps')
-    l0='nom: '+liste[0].user_name+' score: '+str(liste[0].score)+' temps: '+str(liste[0].temps)+' sec'
-    l1='nom: '+liste[1].user_name+' score: '+str(liste[1].score)+' temps: '+str(liste[1].temps)+' sec'
-    l2='nom: '+liste[2].user_name+' score: '+str(liste[2].score)+' temps: '+str(liste[2].temps)+' sec'
+    l0='nom: '+space(liste[0].user_name)+' score: '+str(liste[0].score)+' temps: '+str(liste[0].temps)+' sec'
+    l1='nom: '+space(liste[1].user_name)+' score: '+str(liste[1].score)+' temps: '+str(liste[1].temps)+' sec'
+    l2='nom: '+space(liste[2].user_name)+' score: '+str(liste[2].score)+' temps: '+str(liste[2].temps)+' sec'
     return render(request, 'score/score.html',locals())
